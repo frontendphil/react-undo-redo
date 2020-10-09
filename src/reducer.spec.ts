5b8970933547db250af656a9de3b78032b56a42e
Philipp Giese <giese.philipp+git@gmail.com>
2020-10-09 11:59:15 +0200

    chore: base test setup

---
+++ src/reducer.spec.ts
@@ -0,0 +1,6 @@
+import { reducer } from "./reducer";
+
+describe("reducer", () => {
+  it.todo("initialize the state with an empty past.");
+  it.todo("initialize the state with an empty future.");
+});
